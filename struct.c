fb3b16ec7ac591701c6329432ab3357792e27d11
LemeiLi <strategicfocus@163.com>
2022-05-25 10:21:23 +0800

    array_init.c

--- struct.c
+++ struct.c
@@ -17,21 +17,37 @@ struct spi_handler {
 	struct spi_init init;
 	int tx_counter;
 	int rx_counter;
+	void *priv;
 };
+
 
 int main()
 {
+	int x = 12;
+
+	int a = 10;
+	int *b = &a;
+	int *c = b;
+
+	printf("&a: %p\n", &a);
+	printf("b: %p\n", b);
+	printf("c: %p\n", c);
+
 	struct spi_handler *hspi = &(struct spi_handler) {
 		.init = {
 			.pol = 1,
 			.pol = 0,
 		},
 		.tx_counter = 88,
+		.priv = b,
 	};
 
 	printf("hspi->init.frame_size = %d\n", hspi->init.frame_size);
 	printf("hspi->rx_counter = %d\n", hspi->rx_counter);
 	printf("hspi->tx_counter = %d\n", hspi->tx_counter);
+
+	printf("hspi = %p\n", hspi);
+	printf("hspi->priv val = %p\n", hspi->priv);
 
 	return 0;
 }
