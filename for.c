7ce32310160df5607302b558566debf1964de7ad
LemeiLi <strategicfocus@163.com>
2022-07-01 16:15:50 +0800

    [Description]: for loop condition. [Positioning]: [   Function]: [   Ref-Link]: [        WWH]:

---
+++ for.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: for.c
+	> Author: Caesar
+	> Email: v.manstein@qq.com 
+	> Created Time: Fri 01 Jul 2022 04:14:05 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+	for(int i = 3; i; i--)
+		printf("xx\n");
+
+	return 0;
+}
